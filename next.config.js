84dbb0410114946774ddb60de44c4b0a082892c8
apicfh <api@clubfamilyhotel.app>
2024-11-02 23:38:46 +0100

    Update next.config.js
    add language support

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
+// next.config.js
+module.exports = {
+  i18n: {
+    locales: ['en', 'it'],
+    defaultLocale: 'it', // Imposta italiano come lingua predefinita
+  },
+};
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: process.env.BUILD_STANDALONE === "true" ? "standalone" : undefined,
