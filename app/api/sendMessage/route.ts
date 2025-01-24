ae94ef2fc803b26d96976d92d77f5f9eda0ee320
cfh <api@clubfamilyhotel.app>
2025-01-24 14:20:44 +0100

    Revert "Update route.ts"
    This reverts commit b5c2a90a46925ecf203cfda4ac2025a5265c95bc.

--- app/api/sendMessage/route.ts
+++ app/api/sendMessage/route.ts
@@ -94,7 +94,7 @@ function replaceVarsInTemplate(components: MessageTemplateComponent[], vars: Tem
 }
 
 async function uploadFile(file: File, to: string) {
-    const WHATSAPP_API_URL = `https://graph.facebook.com/v22.0/${process.env.WHATSAPP_API_PHONE_NUMBER_ID}/media`;
+    const WHATSAPP_API_URL = `https://graph.facebook.com/v20.0/${process.env.WHATSAPP_API_PHONE_NUMBER_ID}/media`;
     const headers = {
         'Authorization': `Bearer ${process.env.WHATSAPP_ACCESS_TOKEN}`
     };
@@ -133,7 +133,7 @@ async function uploadFile(file: File, to: string) {
 }
 
 async function sendWhatsAppMessage(to: string, message: string | null | undefined, fileType: string | undefined | null, file: File | undefined | null, template: TemplateRequest | undefined | null) {
-    const WHATSAPP_API_URL = `https://graph.facebook.com/v22.0/${process.env.WHATSAPP_API_PHONE_NUMBER_ID}/messages`;
+    const WHATSAPP_API_URL = `https://graph.facebook.com/v20.0/${process.env.WHATSAPP_API_PHONE_NUMBER_ID}/messages`;
     const payload: Message = {
         messaging_product: "whatsapp",
         recipient_type: "individual",
