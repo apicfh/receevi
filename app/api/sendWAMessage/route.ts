99051120a3645523ccff4c87b033589c61879939
cfh <api@clubfamilyhotel.app>
2025-02-15 19:18:39 +0100

    cors test

--- app/api/sendWAMessage/route.ts
+++ app/api/sendWAMessage/route.ts
@@ -5,13 +5,13 @@ import { createClient } from "@/utils/supabase-server";
 import { TemplateRequest, TextParameter } from "@/types/message-template-request";
 import jwt from 'jsonwebtoken';
 
-export async function POST(request: NextRequest) {
+export async function POST(request: NextRequest, response: NextResponse) {
 
     // Set CORS headers to allow any origin
     // const response = NextResponse.next();
-    // response.headers.set('Access-Control-Allow-Origin', '*');  // Allow requests from any origin
-    // response.headers.set('Access-Control-Allow-Methods', 'POST');  // Allow the necessary HTTP methods
-    // response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');  // Allow the required headers
+    response.headers.set('Access-Control-Allow-Origin', '*');  // Allow requests from any origin
+    response.headers.set('Access-Control-Allow-Methods', 'POST');  // Allow the necessary HTTP methods
+    response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');  // Allow the required headers
 
     const authHeader = request.headers.get('Authorization');
     if (!authHeader) {
--- next.config.js
+++ next.config.js
@@ -8,20 +8,20 @@ module.exports = {
 
 const nextConfig = {
   output: process.env.BUILD_STANDALONE === "true" ? "standalone" : undefined,
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
-          { key: "Access-Control-Allow-Methods", value: "POST" },
-          { key: "Access-Control-Allow-Headers", value: "Content-Type, Authorization" },
-        ]
-      }
-    ]
-  }
+  // async headers() {
+  //   return [
+  //     {
+  //       // matching all API routes
+  //       source: "/api/:path*",
+  //       headers: [
+  //         { key: "Access-Control-Allow-Credentials", value: "true" },
+  //         { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
+  //         { key: "Access-Control-Allow-Methods", value: "POST" },
+  //         { key: "Access-Control-Allow-Headers", value: "Content-Type, Authorization" },
+  //       ]
+  //     }
+  //   ]
+  // }
 }
 
 module.exports = nextConfig
