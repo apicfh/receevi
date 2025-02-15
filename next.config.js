75f39f3cf1a3df7bcfae9fac801a9da28ba88371
cfh <api@clubfamilyhotel.app>
2025-02-15 19:27:34 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ module.exports = {
   },
 };
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: process.env.BUILD_STANDALONE === "true" ? "standalone" : undefined
   // async headers() {
