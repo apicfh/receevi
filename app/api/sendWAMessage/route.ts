85a4858b28270776a247388c2f827b28463edfb2
cfh <api@clubfamilyhotel.app>
2025-02-15 18:39:05 +0100

    vercel json

--- app/api/sendWAMessage/route.ts
+++ app/api/sendWAMessage/route.ts
@@ -8,10 +8,10 @@ import jwt from 'jsonwebtoken';
 export async function POST(request: NextRequest) {
 
     // Set CORS headers to allow any origin
-    const response = NextResponse.next();
-    response.headers.set('Access-Control-Allow-Origin', '*');  // Allow requests from any origin
-    response.headers.set('Access-Control-Allow-Methods', 'POST');  // Allow the necessary HTTP methods
-    response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');  // Allow the required headers
+    // const response = NextResponse.next();
+    // response.headers.set('Access-Control-Allow-Origin', '*');  // Allow requests from any origin
+    // response.headers.set('Access-Control-Allow-Methods', 'POST');  // Allow the necessary HTTP methods
+    // response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');  // Allow the required headers
 
     const authHeader = request.headers.get('Authorization');
     if (!authHeader) {
